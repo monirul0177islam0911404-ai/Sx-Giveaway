1977f680823af46a1217cbcc03a9ed32d0b34437
Md Abdul Malek <monirul0177islam0911404@gmail.com>
2025-12-25 23:59:31 +0600

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,19 +1,5 @@
-import { auth, db } from "./firebase.js";
-import { doc, getDoc, updateDoc }
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-auth.onAuthStateChanged(async u=>{
-  if(!u) return;
-  const ref=doc(db,"users",u.uid);
-  const s=await getDoc(ref);
-  tickets.innerText=s.data().tickets;
-  balance.innerText=s.data().balance;
+auth.onAuthStateChanged(user => {
+  if (!user) {
+    window.location.href = "login.html";
+  }
 });
-
-window.watchAd=async()=>{
-  const u=auth.currentUser;
-  const ref=doc(db,"users",u.uid);
-  const s=await getDoc(ref);
-  await updateDoc(ref,{tickets:s.data().tickets+2});
-  alert("Ticket Added");
-};
